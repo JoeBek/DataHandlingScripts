1aa9bc145402f1d94aadff1e9e606b2f589cc6d4
JoeBek <jb5@vt.edu>
2023-10-03 12:59:19 -0400

    V1.1
    Complete Script! Added matplotlib functionality that correctly graphs the data, as well as writing it to a csv before graphing to be handled elsewhere.

--- physical_electronics_lab2_script.py
+++ physical_electronics_lab2_script.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import math
-import matplotlib as plot
+import matplotlib.pyplot as plot
 import openpyxl as xl
 
 file = 'lab2_physical.csv'
@@ -19,10 +19,37 @@ Vdsq = 2.557
 
 # setting up to change the column
 def Change_Vout(Vout, Vdsq):
-    return Vout - Vdsq
+    return float(Vout) - Vdsq
 
-df['Vout'] = df['Vout'].apply(Change_Vout, args=(Vdsq))
+df['Vout'] = df['Vout'].apply(Change_Vout, args=(Vdsq,))
+
+new_csv = 'physical_lab2_adjusted.csv'
+
+df.to_csv(new_csv, index=False)
 
 
+# create independent columns to graph
+
+time = df['time']
+
+vin = df['Vin']
+
+vout = df['Vout']
+
+fig, ax = plot.subplots()
 
 print(df.head())
+
+ax.plot(time, vin, label='Vin', color='blue')
+ax.plot(time, vout, label='Vout', color='red')
+
+
+ax.set_xlabel('Time')
+ax.set_ylabel('Voltage (V)')
+ax.set_title('Vin and Vout vs Time')
+ax.legend()  # Show the legend with labels
+
+plot.show()
+
+
+
